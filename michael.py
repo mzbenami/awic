e7c6369954be41dc9a1fd8ffd4d24098fbd8f2d0
Michael Ben-Ami <mike.benami@gmail.com>
2014-03-04 12:16:54 -0800

    basic l2 learning

--- michael.py
+++ michael.py
@@ -13,35 +13,93 @@ log = core.getLogger()
 _flood_delay = 0
 
 
-
-
-class LearningSwitch(obect):
-	def __init__ (self, connection, transparent, event):
-		self.connection = connection
+class LearningSwitch(object):
+	def __init__ (self, connection, transparent):
 		self.transparent = transparent
-		self.event = event
+		self.connections = []
+		self.macTable = {}
+		self.addConnection(connection)
+		
+	
+	def addBroadcastFlow(self, event):
+		
+		msg = of.ofp_flow_mod()
+		msg.match = of.ofp_match.from_packet(event.parsed, event.ofp.in_port)
+		msg.data = event.ofp
+		msg.actions.append(of.ofp_action_output(port = of.OFPP_ALL))
+		event.connection.send(msg)
+		
+	def addMacTableFlow(self, event):
+		msg = of.ofp_flow_mod()
+		msg.match = of.ofp_match.from_packet(event.parsed, event.ofp.in_port)
+		msg.data = event.ofp
+		msg.actions.append(of.ofp_action_output(port = self.macTable[event.parsed.dst]))
+		event.connection.send(msg)
 
+	def flood(self, event):
+		print ("flooding!")
+		msg = of.ofp_packet_out()
+		msg.data = event.ofp
+		msg.actions.append(of.ofp_action_output(port = of.OFPP_ALL))
+		event.connection.send(msg)
+
+	def addConnection(self, connection):
+		self.connections.append(connection)
 		connection.addListeners(self)
+	
+	def printPacket(self, packet):
+		if (packet.type == pkt.ethernet.ARP_TYPE):
+			srcip = "arp"
+			dstip = "arp"
+		else:
+			srcip, dstip = packet.payload.srcip, packet.payload.dstip
+		print("Src: {}, Dst: {}, SrcIp: {}, DstIp: {}".format(packet.src, packet.dst, str(srcip), str(dstip)))	
 
-	def _handlePacketIn(self, event):
-		print(dir(event))
-		log.debug("Hello from LearningSwitch")
+	def _handle_PacketIn(self, event):
+		self.lastPacketIn = event
+		packet = event.parsed
 
+		print("handle packet")
+		self.printPacket(packet)
+		
+		self.macTable[packet.src] = event.ofp.in_port
+
+		if packet.dst == EthAddr("FF:FF:FF:FF:FF:FF"):
+			self.addBroadcastFlow(event)
+		elif packet.dst in self.macTable:
+			self.addMacTableFlow(event)
+		else:
+			self.flood(event)
+
+	def _handle_FlowStatsReceived(self, event):
+		print("flow stats received")
+		self.flowstats = event
+		for f in event.stats:
+			if f.match.nw_proto == 1: print f.match.__dict__
+
+	def requestStats(self, connection):
+		connection.send(of.ofp_stats_request(body=of.ofp_flow_stats_request()))
+
+	def requestAllStats(self):
+		for con in self.connections:
+			self.requestStats(con) 
 class l2_learning (object):
-  """
-  Waits for OpenFlow switches to connect and makes them learning switches.
-  """
-  def __init__ (self, transparent):
-    core.openflow.addListeners(self)
-    self.transparent = transparent
+  	"""
+  	Waits for OpenFlow switches to connect and makes them learning switches.
+  	"""
+  	def __init__ (self, transparent):
+    		core.openflow.addListeners(self)
+    		self.transparent = transparent
+		self.my_switch = None
 
-  def _handle_ConnectionUp (self, event):
-    log.debug("Connection %s" % (event.connection,))
-    self.myswitch = LearningSwitch(event.connection, self.transparent, event
-
-  def _handle_PacketIn (self, event):
-  	print(dir(event))
-  	log.debug("Hello from l2_learning!")
+  	def _handle_ConnectionUp (self, event):
+    		log.debug("Connection %s" % (event.connection,))
+		print("handle connection up")
+		self.lastConnectionEvent = event
+		if self.my_switch is None:
+			self.my_switch = LearningSwitch(event.connection, self.transparent)
+		else:
+			self.my_switch.addConnection(event.connection)
 
 def launch (transparent=False, hold_down=_flood_delay):
   """
